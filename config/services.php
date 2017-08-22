8b1304de6c45e92c07be7d85b734b1a42f237f08
md reyad <imam.reyad93@gmail.com>
2017-08-22 23:19:45 +0600

    remove client id

--- config/services.php
+++ config/services.php
@@ -15,23 +15,23 @@ return [
     */
 
     'github' => [
-        'client_id' => 'd1eeba0f29f9f093cd32',
-        'client_secret' => 'd8a9fdd194d7e3aa75e5aabc21a847cf89eed536',
+        'client_id' => env('github_client'),
+        'client_secret' => env('github_secret'),
         'redirect' => 'http://localhost:8000/oauth/github/callback',
     ],
     'facebook' => [
-        'client_id'     => '464015370644613',
-        'client_secret' => '4ef62f60c63ba7b1119354b66ecdfa32',
+        'client_id'     => env('fb_client'),
+        'client_secret' => env('fb_secret'),
         'redirect'      => 'http://localhost:8000/oauth/facebook/callback'
     ],
     'twitter' => [
-        'client_id'     =>'G96W3E0oUTO1cRJplyDfUW2tU',
-        'client_secret' => 'pTHFhluhX59Yd2RPT9a1yEgzZ2uH4WwVkqqFaXm27GKDTtHM7K',
+        'client_id'     =>env('tw_client'),
+        'client_secret' => env('tw_secret'),
         'redirect'      =>'http://localhost:8000/oauth/twitter/callback',
     ],
     'google' => [
-        'client_id'     =>'946883670013-khem1gab1putafur5hrdppus965b49ft.apps.googleusercontent.com',
-        'client_secret' => 'j04CAHLcq0Yi70J_yGW6UBF6',
+        'client_id'     =>env('go_client'),
+        'client_secret' => env('go_secret'),
         'redirect'      =>'http://localhost:8000/oauth/google/callback',
     ],
 
